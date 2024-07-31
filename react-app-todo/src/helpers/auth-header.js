7e11dcde897679842092f2b35af015d3bfe7c2f7
jounetsuST <sanhtam.lam@gmail.com>
2024-07-31 10:42:45 +0200

    update

--- react-app-todo/src/helpers/auth-header.js
+++
@@ -1,8 +0,0 @@
-export function authHeader() {
-  const user = JSON.parse(localStorage.getItem("user"));
-  if (user && user.data.token) {
-    return { Authorization: "Bearer " + user.data.token };
-  } else {
-    return {};
-  }
-}
--- spring_security_todo/src/main/java/org/example/spring_security/enums/StateEnum.java
+++
@@ -1,6 +0,0 @@
-package org.example.spring_security.enums;
-
-public enum StateEnum {
-    COMPLETED,
-    IN_PROGRESS
-}
